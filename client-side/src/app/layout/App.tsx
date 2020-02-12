0520a668a744cec29e34d178192609d6e13dd227
github username <s.xue.bai@gmail.com>
2020-02-11 23:10:39 -0600

    structured react folders

--- client-side/src/app/layout/App.tsx
+++ client-side/src/app/layout/App.tsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Header, Icon } from "semantic-ui-react";
 import { List } from "semantic-ui-react";
-import "./App.css";
 import axios from "axios";
 class App extends Component {
   state = { values: [] };
